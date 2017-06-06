5c064bf5a1c34986dd4ee1bd5d7f365143f4b6da
耿小泰 <clawpo@foxmail.com>
2017-06-06 17:01:49 +0800

    公开群的搜索功能

--- superwechat/src/cn/ucai/superwechat/ui/PublicGroupsSeachActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/PublicGroupsSeachActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,32 +14,43 @@ import android.widget.Toast;
 import com.hyphenate.EMError;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMGroup;
-import cn.ucai.superwechat.R;
+import com.hyphenate.easeui.utils.EaseUserUtils;
 import com.hyphenate.exceptions.HyphenateException;
+
+import cn.ucai.superwechat.R;
 
 public class PublicGroupsSeachActivity extends BaseActivity{
     private RelativeLayout containerLayout;
     private EditText idET;
     private TextView nameText;
+    private ImageView avatar;
     public static EMGroup searchedGroup;
 
     @Override
     protected void onCreate(Bundle arg0) {
-        super.onCreate(arg0);
         setContentView(R.layout.em_activity_public_groups_search);
+        super.onCreate(arg0);
+        showLeftBack();
+        setListener();
         
         containerLayout = (RelativeLayout) findViewById(R.id.rl_searched_group);
         idET = (EditText) findViewById(R.id.et_search_id);
         nameText = (TextView) findViewById(R.id.name);
+        avatar = (ImageView)findViewById(R.id.avatar);
         
         searchedGroup = null;
     }
-    
-    /**
-     * search group with group id
-     * @param v
-     */
-    public void searchGroup(View v){
+
+    private void setListener() {
+        titleBar.getRightLayout().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchGroup();
+            }
+        });
+    }
+
+    public void searchGroup(){
         if(TextUtils.isEmpty(idET.getText())){
             return;
         }
@@ -58,6 +70,8 @@ public class PublicGroupsSeachActivity extends BaseActivity{
                             pd.dismiss();
                             containerLayout.setVisibility(View.VISIBLE);
                             nameText.setText(searchedGroup.getGroupName());
+                            EaseUserUtils.setGroupAvatarByHxid(PublicGroupsSeachActivity.this,
+                                    searchedGroup.getGroupId(),avatar);
                         }
                     });
                     
